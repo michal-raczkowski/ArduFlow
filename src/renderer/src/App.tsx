623959c5ba60ec40b5ae49e474c98dba92ecf602
Michal Raczkowski <wertyksonred@gmail.com>
2023-06-06 05:54:14 +0200

    Inital

---
+++ src/renderer/src/App.tsx
@@ -0,0 +1,19 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div>
+        <div className="exampleChart"></div>
+      </div>
+      <div className="card">
+        <p>
+          Test of <code>ArduFlow</code> library
+        </p>
+      </div>
+      <p className="read-the-docs">Project to upload sketches to arduino</p>
+    </>
+  )
+}
+
+export default App
